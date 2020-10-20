ac377b34de06af5962489284ddb0493c6cf42a4f
yangxinmei <1099061022@qq.com>
2020-10-20 17:47:20 +0800

    fix: 调整菜单数据

--- src/page/common/menuData.js
+++ src/page/common/menuData.js
@@ -1,5 +1,27 @@
 let menuData = [
   {
+    name: '基础组件',
+    children: [
+      {
+        name: '简单弹框',
+        path: '/basicModule/dialog',
+      },
+      {
+        name: '表单弹框',
+        path: '/basicModule/dialog',
+      }
+    ]
+  },
+  {
+    name: '对象数组',
+    children: [
+      {
+        name: '去重',
+        path: '/objectArr/uniq',
+      }
+    ]
+  },
+  {
     name: '模板示例',
     path: '/mei/demo',
     children: []
@@ -8,188 +30,6 @@ let menuData = [
     name: '模板示例2',
     path: '/mei/demo2',
     children: []
-  },
-  {
-    name: '菜单二',
-    path: '/other',
-    children: [
-      {
-        name: '子菜单一',
-        path: 'other/1',
-      },
-      {
-        name: '子菜单二',
-        path: 'other/2'
-      }
-    ]
-  },
-  {
-    name: '菜单二',
-    path: '/other',
-    children: [
-      {
-        name: '子菜单一',
-        path: 'other/1',
-      },
-      {
-        name: '子菜单二',
-        path: 'other/2'
-      }
-    ]
-  },
-  {
-    name: '菜单二',
-    path: '/other',
-    children: [
-      {
-        name: '子菜单一',
-        path: 'other/1',
-      },
-      {
-        name: '子菜单二',
-        path: 'other/2'
-      }
-    ]
-  },
-  {
-    name: '菜单二',
-    path: '/other',
-    children: [
-      {
-        name: '子菜单一',
-        path: 'other/1',
-      },
-      {
-        name: '子菜单二',
-        path: 'other/2'
-      }
-    ]
-  },
-  {
-    name: '菜单二',
-    path: '/other',
-    children: [
-      {
-        name: '子菜单一',
-        path: 'other/1',
-      },
-      {
-        name: '子菜单二',
-        path: 'other/2'
-      }
-    ]
-  },
-  {
-    name: '菜单二',
-    path: '/other',
-    children: [
-      {
-        name: '子菜单一',
-        path: 'other/1',
-      },
-      {
-        name: '子菜单二',
-        path: 'other/2'
-      }
-    ]
-  },
-  {
-    name: '菜单二',
-    path: '/other',
-    children: [
-      {
-        name: '子菜单一',
-        path: 'other/1',
-      },
-      {
-        name: '子菜单二',
-        path: 'other/2'
-      }
-    ]
-  },
-  {
-    name: '菜单二',
-    path: '/other',
-    children: [
-      {
-        name: '子菜单一',
-        path: 'other/1',
-      },
-      {
-        name: '子菜单二',
-        path: 'other/2'
-      }
-    ]
-  },
-  {
-    name: '菜单二',
-    path: '/other',
-    children: [
-      {
-        name: '子菜单一',
-        path: 'other/1',
-      },
-      {
-        name: '子菜单二',
-        path: 'other/2'
-      }
-    ]
-  },
-  {
-    name: '菜单二',
-    path: '/other',
-    children: [
-      {
-        name: '子菜单一',
-        path: 'other/1',
-      },
-      {
-        name: '子菜单二',
-        path: 'other/2'
-      }
-    ]
-  },
-  {
-    name: '菜单二',
-    path: '/other',
-    children: [
-      {
-        name: '子菜单一',
-        path: 'other/1',
-      },
-      {
-        name: '子菜单二',
-        path: 'other/2'
-      }
-    ]
-  },
-  {
-    name: '菜单二',
-    path: '/other',
-    children: [
-      {
-        name: '子菜单一',
-        path: 'other/1',
-      },
-      {
-        name: '子菜单二',
-        path: 'other/2'
-      }
-    ]
-  },
-  {
-    name: '菜单二',
-    path: '/other',
-    children: [
-      {
-        name: '子菜单一',
-        path: 'other/1',
-      },
-      {
-        name: '子菜单二',
-        path: 'other/2'
-      }
-    ]
   },
   {
     name: '菜单二',
