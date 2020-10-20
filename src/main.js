ff6b6a012b0e630fb44befe742810b930afb139d
yangxinmei <1099061022@qq.com>
2020-10-20 17:48:50 +0800

    feat: 引入自定义样式 以及 vuex

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from './router/index'
 import elementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
 import monaco from './components/vue-monaco-editor/src/Monaco.vue'
+// 自定义样式
+import './style/css/index.js'
+import store from './store'
 
 Vue.component('monaco', monaco)
 Vue.use(elementUI)
@@ -12,5 +15,6 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app')
