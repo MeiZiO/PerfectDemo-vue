d96712baf12266fcafd7b3506e6f0f2839589a35
yangxinmei <yangxinmei@beyondcent.com>
2021-10-11 14:42:21 +0800

    update: 更新菜单

--- src/common/menuData.js
+++ src/common/menuData.js
@@ -34,30 +34,6 @@ let menuData = [
     name: '异常汇总',
     path: '/allBug',
     children: []
-  },
-  {
-    name: '模板示例',
-    path: '/mei/demo',
-    children: []
-  },
-  {
-    name: '模板示例2',
-    path: '/mei/demo2',
-    children: []
-  },
-  {
-    name: '菜单二',
-    path: '/other',
-    children: [
-      {
-        name: '子菜单一',
-        path: 'other/1',
-      },
-      {
-        name: '子菜单二',
-        path: 'other/2'
-      }
-    ]
   }
 ]
 export default menuData
