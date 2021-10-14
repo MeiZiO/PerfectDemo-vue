7bc1db97188b7492d7546cdcf8bb41901d6c9deb
yangxinmei <yangxinmei@beyondcent.com>
2021-10-14 16:57:50 +0800

    fix: 补充细节

--- src/codes/functional/summaryInfo/show.js
+++ src/codes/functional/summaryInfo/show.js
@@ -2,6 +2,13 @@ export let code =
 `
 <template>
     <el-card class="showCard">
+        <!-- dataList是展示的卡片数组,typeList是筛选类型,colSpan是每一个卡片的宽度取值0-24 -->
+        <el-row>
+            <el-col :span="3">卡片宽度:</el-col>
+            <el-col :span="8">
+                <el-slider v-model="colSpan" :max=24 show-input></el-slider>
+            </el-col>
+        </el-row>
         <summaryInfo :dataList="info" :typeList='typeList' :colSpan='8'></summaryInfo>
     </el-card>
 </template>
@@ -17,7 +24,8 @@ export default {
     data () {
         return {
             info,
-            typeList:['测试类型一', '测试类型二']
+            typeList:['测试类型一', '测试类型二'],
+            colSpan: 8,
         }
     }
 }
--- src/page/functional/summaryInfo/show.vue
+++ src/page/functional/summaryInfo/show.vue
@@ -1,6 +1,13 @@
 <template>
     <el-card class="showCard">
-        <summaryInfo :dataList="info" :typeList='typeList' :colSpan='8'></summaryInfo>
+        <!-- dataList是展示的卡片数组,typeList是筛选类型,colSpan是每一个卡片的宽度取值0-24 -->
+        <el-row>
+            <el-col :span="3">卡片宽度:</el-col>
+            <el-col :span="8">
+                <el-slider v-model="colSpan" :max=24 show-input></el-slider>
+            </el-col>
+        </el-row>
+        <summaryInfo :dataList="info" :typeList='typeList' :colSpan='colSpan'></summaryInfo>
     </el-card>
 </template>
 
@@ -10,12 +17,13 @@ import summaryInfo from './index.vue'
 
 export default {
     components: {
-        summaryInfo
+        summaryInfo,
     },
     data () {
         return {
             info,
-            typeList:['测试类型一', '测试类型二']
+            typeList:['测试类型一', '测试类型二'],
+            colSpan: 8,
         }
     },
      mounted() {
